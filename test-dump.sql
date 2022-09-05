8abb8c878795bc18d257357884f912435e9e48ac
a1saipavan <76589153+a1saipavan@users.noreply.github.com>
2022-09-05 15:54:54 +0530

    vc,

--- test-dump.sql
+++ test-dump.sql
@@ -1,8 +1,18 @@
-CREATE TABLE "foo" (
-    "id" serial,
-    "name" text
+CREATE TABLE uk_property_registry(
+  transaction uuid,
+  price numeric,
+  transfer_date date,
+  postcode text,
+  property_type char(1),
+  newly_built boolean,
+  duration char(1),
+  paon text,
+  saon text,
+  street text,
+  locality text,
+  city text,
+  district text,
+  county text,
+  ppd_category_type char(1),
+  record_status char(1)
 );
-
-INSERT INTO "foo" ("name") VALUES
-  ("peter"), ("jesus"), ("andy")
-;
